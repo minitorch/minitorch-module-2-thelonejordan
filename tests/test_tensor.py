ffdd9045ebb20e7e76872d3af531c6f1d2d90d15
Jyotirmaya Mahanta <jyotirmaya.mahanta@coupa.com>
2023-12-24 14:55:42 +0530

    format

--- tests/test_tensor.py
+++ tests/test_tensor.py
@@ -29,7 +29,16 @@ def test_one_args(
     "Test one-arg functions compared to floats"
     name, base_fn, tensor_fn = fn
     t2 = tensor_fn(t1)
-    print('debug', tensor_fn, t1, t1._tensor.shape, t1._tensor.strides, t2, t2._tensor.shape, t2._tensor.strides)
+    print(
+        "debug",
+        tensor_fn,
+        t1,
+        t1._tensor.shape,
+        t1._tensor.strides,
+        t2,
+        t2._tensor.shape,
+        t2._tensor.strides,
+    )
     for ind in t2._tensor.indices():
         assert_close(t2[ind], base_fn(t1[ind]))
 
